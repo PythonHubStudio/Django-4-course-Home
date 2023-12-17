0a68343c48b8a8ba13e19c2f130011b0f0dd1c09
PythonHubStudio <76648932+PythonHubStudio@users.noreply.github.com>
2023-12-17 11:28:55 +0200

    Second pagination option - GET params

--- goods/urls.py
+++ goods/urls.py
@@ -6,7 +6,6 @@ app_name = 'goods'
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name='index'),
-    path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
 
--- goods/views.py
+++ goods/views.py
@@ -4,14 +4,17 @@ from django.shortcuts import get_list_or_404, get_object_or_404, render
 from goods.models import Products
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    
+    page = request.GET.get('page', 1)
+    
     if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "Home - Каталог",
