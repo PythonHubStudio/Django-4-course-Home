245ea80411a79cff02045faa98328f4e0878da3c
PythonHubStudio <76648932+PythonHubStudio@users.noreply.github.com>
2023-12-17 11:29:25 +0200

    login required decorator - redirect to target page

--- users/views.py
+++ users/views.py
@@ -17,6 +17,10 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                    
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
