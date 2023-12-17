aec3492969d8282338615950aa82257625bb48a3
PythonHubStudio <76648932+PythonHubStudio@users.noreply.github.com>
2023-12-17 11:29:04 +0200

    PostgreSQL data base connected & full text search

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     "debug_toolbar",
 
@@ -85,8 +86,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': 'home',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from goods.models import Products
 
@@ -7,13 +8,19 @@ def q_search(query):
     
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
-    keywords = [word for word in query.split() if len(word) > 2]
 
-    q_objects = Q()
+    # keywords = [word for word in query.split() if len(word) > 2]
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    # q_objects = Q()
 
-    return Products.objects.filter(q_objects)
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+
+    # return Products.objects.filter(q_objects)
