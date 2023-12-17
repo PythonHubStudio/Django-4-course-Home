b860add6ee7c9b3e6e4e778eeb76e3fde6c97cc5
PythonHubStudio <76648932+PythonHubStudio@users.noreply.github.com>
2023-12-17 11:28:52 +0200

    The first pagination option

--- goods/models.py
+++ goods/models.py
@@ -29,6 +29,7 @@ class Products(models.Model):
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+        ordering = ("id",)
 
     def __str__(self):
         return f'{self.name} Количество - {self.quantity}'
--- goods/views.py
+++ goods/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from goods.models import Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
     if category_slug == "all":
         goods = Products.objects.all()
     else:
-        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
 
     context = {
         "title": "Home - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug
     }
     return render(request, "goods/catalog.html", context)
 
