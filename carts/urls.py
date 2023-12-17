d0f89f46cde659b71e022ec06a170d557c2a6005
PythonHubStudio <76648932+PythonHubStudio@users.noreply.github.com>
2023-12-17 11:29:34 +0200

    Cart remove controller added, commit before ajax

--- carts/urls.py
+++ carts/urls.py
@@ -7,5 +7,5 @@ app_name = 'carts'
 urlpatterns = [
     path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'),
     path('cart_change/<slug:product_slug>/', views.cart_change, name='cart_change'),
-    path('cart_remove/<slug:product_slug>/', views.cart_remove, name='cart_remove'),
+    path('cart_remove/<int:cart_id>/', views.cart_remove, name='cart_remove'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -25,5 +25,9 @@ def cart_add(request, product_slug):
 def cart_change(request, product_slug):
     ...
 
-def cart_remove(request, product_slug):
-    ...
+def cart_remove(request, cart_id):
+    
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    
+    return redirect(request.META['HTTP_REFERER'])
